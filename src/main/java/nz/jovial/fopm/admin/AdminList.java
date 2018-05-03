5e1b01fdd630c1fbc48db3cb1a0a3c4f9a736b44
Paul Reilly <paldiutheminecrafter@gmail.com>
2018-05-03 15:42:25 -0400

    stuff

--- src/main/java/nz/jovial/fopm/admin/AdminList.java
+++ src/main/java/nz/jovial/fopm/admin/AdminList.java
@@ -86,7 +86,6 @@ public class AdminList
             }
 
             admins.add(admin);
-            admin.save();
         });
 
         FLog.info("Successfully loaded " + admins.size() + " admins!");
---
+++ src/main/java/nz/jovial/fopm/command/Command_nick.java
@@ -0,0 +1,59 @@
+package nz.jovial.fopm.command;
+
+import nz.jovial.fopm.admin.AdminList;
+import nz.jovial.fopm.rank.Rank;
+import nz.jovial.fopm.util.FUtil;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandParameters(description="Change your or another's nickname.", usage="/<command> <nickname | off> [player]", source=SourceType.IN_GAME, rank=Rank.OP)
+public class Command_nick {
+    public boolean onCommand(CommandSender sender, Command cmd, String lbl, String[] args) {
+        
+        if ((args.length < 1) || (args.length > 2)) {
+            return false;
+        }
+        
+        Player p = (Player) sender;
+        int maxCharLimit = 16;
+        if (args.length == 1) {
+            if (args[0].equalsIgnoreCase("off")) {
+                p.setDisplayName(p.getName());
+                return true;
+            }
+            if (maxCharLimit < args[0].length()) {
+                p.sendMessage(ChatColor.YELLOW + "ERROR: Your nickname can only be a maximum of 16 characters.");
+                return true;
+            }
+            p.setDisplayName(FUtil.colorize(args[0]));
+        }
+        
+        if (args.length == 2) {
+            if (AdminList.isAdmin(p)) {
+                Player t = Bukkit.getPlayer(args[1]);
+                if (t == null) {
+                    p.sendMessage(ChatColor.YELLOW + "ERROR: That player is not online!");
+                    return true;
+                }
+
+                if (args[0].equalsIgnoreCase("off")) {
+                    t.setDisplayName(t.getName());
+                    return true;
+                }
+                
+                if (args[0].length() > maxCharLimit) {
+                    p.sendMessage(ChatColor.YELLOW + "ERROR: That nickname is too long!");
+                    return true;
+                }
+                
+                t.setDisplayName(args[0]);
+                return true;
+            }
+        }
+        
+        return true;
+    }
+}
